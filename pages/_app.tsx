d7c454663295a0cb7453a77c8d58b2bf5f5a0e61
sagarjadhavwebdev <sagarjadhavlive@gmail.com>
2021-04-10 21:19:03 +0530

    sw added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,26 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './componenets/Header'
 import Footer from './componenets/Footer'
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+  
+    if("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+       navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, [])
   return (
     <>
     <Head>

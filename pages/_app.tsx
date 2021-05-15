baf5d3047b4d6c4ab17cf2d79911faab746d7978
sagarjadhavwebdev <sagarjadhavlive@gmail.com>
2021-05-15 22:37:07 +0530

    fghjkl

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,21 +5,8 @@ import Header from './componenets/Header'
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-  
-    if("serviceWorker" in navigator) {
-      window.addEventListener("load", function () {
-       navigator.serviceWorker.register("/sw.js").then(
-          function (registration) {
-            console.log("Service Worker registration successful with scope: ", registration.scope);
-          },
-          function (err) {
-            console.log("Service Worker registration failed: ", err);
-          }
-        );
-      });
-    }
-  }, [])
+ 
+
   return (
     <>
     <Head >

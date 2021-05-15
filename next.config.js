ceb96fc81ec13c2ef323b91e2cf0ff107e46b5d8
sagarjadhavwebdev <sagarjadhavlive@gmail.com>
2021-05-15 23:26:47 +0530

    sdfgg

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const withPWA = require('next-pwa')
+
 module.exports = withPWA({
-    future: {
-      webpack5: true,
-    },
-    images: {
-      domains: ['https://source.unsplash.com'],
-    },
-  })
+  pwa: {
+    dest: 'public',
+    register: true,
+    scope: '/app',
+    sw: 'service-worker.js',
+    
+  }
+})

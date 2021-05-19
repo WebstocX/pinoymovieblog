d2060bcaaf542365bca19c93d924f31ba66aced5
sagarjadhavwebdev <sagarjadhavlive@gmail.com>
2021-05-20 00:01:15 +0530

    m,.hjkl;'

--- pages/componenets/atoms/Hero.tsx
+++ pages/componenets/atoms/Hero.tsx
@@ -14,7 +14,7 @@ console.log("Hero", tag)
   return (
     <>
       <section data-aos="fade-left" data-aos-duration="2000" style={{backgroundColor:sectionstyle}} className="text-gray-600 body-font">
-        <div className="container md:px-56 md:py-12 py-10 mx-auto">
+        <div data-aos="fade-left" data-aos-duration="2000" className="container md:px-56 md:py-12 py-10 mx-auto">
           <div className="flex flex-col text-center justify-center items-center w-full mb-8">
             <span className="text-indigo-500 md:text-xl textt-center mb-2 ">
               {tag}

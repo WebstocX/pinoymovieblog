86a92480f779a3a65a700a353cf62130848b7935
WebstocX <webstocx@gmail.com>
2021-05-24 22:21:22 +0530

    Add keyword and description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,8 +23,8 @@ function MyApp({ Component, pageProps }) {
 
       <meta http-equiv='X-UA-Compatible' content='IE=edge' />
       <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
-      <meta name='description' content='WebStocx Digital Marketing Agency ' />
-      <meta name='keywords' content='Keywords' />
+      <meta name='description' content='Website Design and Development Services Develop your Website with us in pocket friendly budget, We provide you great innovative to grow your business and sales with our strategies. ' />
+      <meta name='keywords' content='Website Development, Website Design, Website Development Agency, Website Making, Business Website Development, Low cost Website Development ' />
       <link rel='manifest' href='/manifest.json' />
       <link rel="preconnect" href="https://fonts.gstatic.com"/>
        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
@@ -32,7 +32,7 @@ function MyApp({ Component, pageProps }) {
       <meta name='theme-color' content='#317EFB' />
         <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"></link>
         <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
-       
+        <meta name="google-site-verification" content="8gmB9T9e8CwgviKIOsGfCCZPO_slq_vfHPPOIVsLjik" />
      </Head>
     <Header/>
     <Component   {...pageProps} />
